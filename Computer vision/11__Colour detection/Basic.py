4836d5d2bbf93d40a788f91cdf42180a78196b7d
Programmer-Rakesh <rakeshaps8866@gmail.com>
2025-03-15 04:13:46 +0530

    Damnn amazing files

---
+++ Computer vision/11__Colour detection/Basic.py
@@ -0,0 +1,33 @@
+
+import cv2
+import numpy as np
+from util import get_limits
+
+yellow = np.array([0, 255, 255])
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to grab frame")
+        continue
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=yellow)
+
+    cv2.imshow('Original', frame)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    cv2.imshow('frame', mask)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
